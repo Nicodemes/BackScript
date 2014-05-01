b2e3a9e959ada67cf824b7430cab17679efd276d
Nicodemes <sasha@paticon.com>
2014-05-01 12:55:35 +0300

    Starting the multi FIle Support

---
+++ Interaptor/MultiFileControler.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter {
+    //this class will menage all the files.
+}
